bce8d46c5359919b0aee5055b77bef08e5ee9ed5
ThePrimeagen <the.primeagen@aol.com>
2025-05-04 17:30:22 +0200

    B: Added circle and player

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+from player import Player
 from constants import *
 
-pygame.init()
+
 
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -11,7 +16,12 @@ def main():
                 return
             
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        #limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
